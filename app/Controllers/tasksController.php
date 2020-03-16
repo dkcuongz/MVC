febc034108406eb8cb77cb55d93138daa9daccb1
Dk Cường <dkcuongz@gmail.com>
2020-03-16 11:10:59 +0700

    first commit

---
+++ app/Controllers/tasksController.php
@@ -0,0 +1,74 @@
+<?php
+namespace App\Controllers;
+
+require "../vendor/autoload.php";
+
+use App\Core\Controller;
+use App\Models\Task ;
+
+class tasksController extends Controller
+{
+    // public $task;
+    //     public function __construct()
+    //     {
+    //         $this->task = new Task();
+    //     }
+
+    function index()
+    {
+        // require(ROOT . 'app/Models/Task.php');
+
+        $tasks = new Task();
+
+        $d['tasks'] = $tasks->showAllTasks();
+        $this->set($d);
+        $this->render("index");
+    }
+
+    function create()
+    {
+        if (isset($_POST["title"]))
+        {
+        //    require(ROOT . 'app/Models/Task.php');
+
+            $task= new Task();
+
+            if ($task->create($_POST["title"], $_POST["description"]))
+            {
+                header("Location: " . WEBROOT . "tasks/index");
+            }
+        }
+
+        $this->render("create");
+    }
+
+    function edit($id)
+    {
+        // require(ROOT . 'app/Models/Task.php');
+        $task= new Task();
+
+        $d["task"] = $task->showTask($id);
+
+        if (isset($_POST["title"]))
+        {
+            if ($task->edit($id, $_POST["title"], $_POST["description"]))
+            {
+                header("Location: " . WEBROOT . "tasks/index");
+            }
+        }
+        $this->set($d);
+        $this->render("edit");
+    }
+
+    function delete($id)
+    {
+    //    require(ROOT . 'app/Models/Task.php');
+
+        $task = new Task();
+        if ($task->delete($id))
+        {
+            header("Location: " . WEBROOT . "tasks/index");
+        }
+    }
+}
+?>
